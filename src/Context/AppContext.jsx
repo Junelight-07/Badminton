73be0162056ab597252c49962f1560350eae079e
Alexy <alexyddb@gmail.com>
2024-03-28 17:57:50 +0100

    adding proptypes

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,9 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 const AppContext = React.createContext({ isLoggedIn: false, isAdmin: false });
 
 export const useAppContext = () => useContext(AppContext);
 
+AppContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default function AppContextProvider({ children }) {
   const token = localStorage.getItem("token");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
